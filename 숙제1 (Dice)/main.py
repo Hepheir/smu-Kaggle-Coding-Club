9ebb734a226ec36f52f92df09577bafe3715d5f9
Hepheir <hepheir@gmail.com>
2023-03-27 07:02:14 +0900

    feat: impl main()

--- 숙제1 (Dice)/main.py
+++ 숙제1 (Dice)/main.py
@@ -1,5 +1,11 @@
+from dice import DiceProbability
+
+
 def main():
-    pass
+    n = int(input("주사위를 굴릴 횟수(n)을 입력: "))
+    diceProbability = DiceProbability(n)
+    diceProbability.calcProbability()
+    diceProbability.printProbability()
 
 
 if __name__ == '__main__':
